132d7eb89353d3f6703efa0d0d3db86e27cc6ab7
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-02-04 23:45:03 +0000

    Fix efetch bugs

--- compact_efetch.py
+++ compact_efetch.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 def extract_pubmed_articles(path: Path, pubmed_article_set_path: str):
     try:
         tree = ET.parse(pubmed_article_set_path)
-    except ET.XMLSyntaxError:
+    except ET.ParseError:
         try:
             Path(pubmed_article_set_path).unlink()
         except FileNotFoundError:
@@ -21,6 +21,8 @@ def extract_pubmed_articles(path: Path, pubmed_article_set_path: str):
         return
     root = tree.getroot()
 
+    pubmed_article_dir = path / 'efetch/PubmedArticle'
+    pubmed_article_dir.mkdir(parents=True, exist_ok=True)
     csv_dir = path / 'csv'
     csv_dir.mkdir(parents=True, exist_ok=True)
 
@@ -28,7 +30,7 @@ def extract_pubmed_articles(path: Path, pubmed_article_set_path: str):
         pmid = pubmed_article.find('MedlineCitation/PMID').text
         pubmed_article_stem = Path(pubmed_article_set_path).stem
         csv_path = csv_dir / f"{pubmed_article_stem}.csv"
-        pubmed_article_path = path / 'efetch/PubmedArticle' / f"{pmid}.xml"
+        pubmed_article_path = pubmed_article_dir / f"{pmid}.xml"
         if not csv_path.exists():
             with open(csv_path, 'w') as f:
                 f.write('pubmed_article_set, pubmed_article\n')
