cafca2827f0f1d02667620105af697d5174fce58
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-04-02 23:16:26 +0000

    Add log_path argument

--- sync.py
+++ sync.py
@@ -144,7 +144,7 @@ def get_remote_dates(ftp_path: str, group: str):
 
 async def sync_incrs(ftp_path: str, local_path: str, group: str):
     local_latest_dates = get_local_dates(local_path, group)
-    remote_latest_dates = get_remote_dates(group)
+    remote_latest_dates = get_remote_dates(ftp_path, group)
     dates_delta = sorted(set(remote_latest_dates) - set(local_latest_dates))
     if not dates_delta:
         logger.info(f"Already up to date for group {group} incrs")
@@ -152,12 +152,25 @@ async def sync_incrs(ftp_path: str, local_path: str, group: str):
         logger.info(f"Dates delta: {dates_delta}")
     sem = Semaphore(1)
     for date in tqdm(dates_delta):
-        await sync_incr(date.isoformat(), group, sem)
+        await sync_incr(ftp_path, local_path, date.isoformat(), group, sem)
+
+
+def setup_logger(log_path: Path):
+    log_path.mkdir(parents=True, exist_ok=True)
+    filename = log_path / f"{Path(__file__).stem}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+    logging.basicConfig(
+        filename=filename,
+        filemode="w",
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+    )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    logger.addHandler(console)
 
 
 async def main():
-    # TODO: logs broken..
-    # TODO: highly duplicated code
+    # TODO: remove code duplication
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--ftp_path",
@@ -171,21 +184,19 @@ async def main():
         default="/data/ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk",
         help="Local path to download PMC OA bulk",
     )
+    parser.add_argument(
+        "--log_path",
+        type=str,
+        default="logs",
+        help="Path to store logs",
+    )
     args = parser.parse_args()
     ftp_path = args.ftp_path
     local_path = args.local_path
+    log_path = Path(args.log_path)
 
-    Path("logs").mkdir(parents=True, exist_ok=True)
-    filename = f"logs/{Path(__file__).stem}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
-    logging.basicConfig(
-        filename=filename,
-        filemode="w",
-        level=logging.INFO,
-        format="%(asctime)s %(levelname)s %(message)s",
-    )
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    logger.addHandler(console)
+    setup_logger(log_path)
+
     groups = ["comm", "noncomm", "other"]
     for group in groups:
         await sync_baselines(ftp_path, local_path, group)
