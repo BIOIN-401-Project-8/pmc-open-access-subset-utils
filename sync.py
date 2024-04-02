cdcb9a24c6149837cd8a5995c57167f1e2e3ed7c
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-04-02 22:44:38 +0000

    Refactor to use argparse

--- sync.py
+++ sync.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import glob
 import logging
@@ -14,14 +15,12 @@ import aiohttp
 from tqdm import tqdm
 from tqdm.asyncio import tqdm_asyncio
 
-FTP_PATH = "https://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk"
-LOCAL_PATH = "/data/ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk"
 logger = logging.getLogger(__name__)
 
 
-async def sync_baselines(group: str):
-    local_csvs = get_local_baselines(group)
-    remote_csvs = get_remote_baselines(group)
+async def sync_baselines(ftp_path: str, local_path: str, group: str):
+    local_csvs = get_local_baselines(local_path, group)
+    remote_csvs = get_remote_baselines(ftp_path, group)
     csvs_delta = sorted(set(remote_csvs) - set(local_csvs))
     targz_delta = [csv.replace("filelist.csv", "tar.gz") for csv in csvs_delta]
     logger.info(f"CSVs delta: {csvs_delta}")
@@ -29,12 +28,16 @@ async def sync_baselines(group: str):
         logger.info(f"Already up to date for group {group} baselines")
     sem = Semaphore(1)
     await tqdm_asyncio.gather(
-        *[sync_baseline(group, csv, targz, sem) for csv, targz in list(zip(csvs_delta, targz_delta))]
+        *[
+            sync_baseline(ftp_path, local_path, group, csv, targz, sem)
+            for csv, targz in list(zip(csvs_delta, targz_delta))
+        ]
     )
 
-async def sync_baseline(group: str, csv: str, targz: str, sem: Semaphore):
+
+async def sync_baseline(ftp_path: str, local_path: str, group: str, csv: str, targz: str, sem: Semaphore):
     with tempfile.NamedTemporaryFile() as tmp:
-        url = f"{FTP_PATH}/oa_{group}/xml/{targz}"
+        url = f"{ftp_path}/oa_{group}/xml/{targz}"
         timeout = aiohttp.ClientTimeout(total=None)
         async with sem, aiohttp.ClientSession(timeout=timeout) as session:
             logger.info(f"{url} -> {tmp.name}")
@@ -45,8 +48,8 @@ async def sync_baseline(group: str, csv: str, targz: str, sem: Semaphore):
                             await f.write(chunk)
                 else:
                     raise Exception(f"Failed to download {url}")
-        Path(f"{LOCAL_PATH}/oa_{group}/xml/").mkdir(parents=True, exist_ok=True)
-        args = ["tar", "-xzvf", tmp.name, "-C", f"{LOCAL_PATH}/oa_{group}/xml/"]
+        Path(f"{local_path}/oa_{group}/xml/").mkdir(parents=True, exist_ok=True)
+        args = ["tar", "-xzvf", tmp.name, "-C", f"{local_path}/oa_{group}/xml/"]
         logger.info(" ".join(args))
         process = await asyncio.create_subprocess_exec(
             *args,
@@ -57,9 +60,9 @@ async def sync_baseline(group: str, csv: str, targz: str, sem: Semaphore):
         if process.returncode != 0:
             logger.error(stdout)
             logger.error(stderr)
-            raise Exception(f"Failed to extract {tmp.name} to {LOCAL_PATH}/oa_{group}/xml/")
-    url = f"{FTP_PATH}/oa_{group}/xml/{csv}"
-    filename = f"{LOCAL_PATH}/oa_{group}/xml/{csv}"
+            raise Exception(f"Failed to extract {tmp.name} to {local_path}/oa_{group}/xml/")
+    url = f"{ftp_path}/oa_{group}/xml/{csv}"
+    filename = f"{local_path}/oa_{group}/xml/{csv}"
     logger.info(f"{url} -> {filename}")
     timeout = aiohttp.ClientTimeout(total=None)
     async with aiohttp.ClientSession(timeout=timeout) as session:
@@ -71,23 +74,23 @@ async def sync_baseline(group: str, csv: str, targz: str, sem: Semaphore):
                 raise Exception(f"Failed to download {url}")
 
 
-def get_remote_baselines(group: str):
-    with urllib.request.urlopen(f"{FTP_PATH}/oa_{group}/xml/") as response:
+def get_remote_baselines(ftp_path: str, group: str):
+    with urllib.request.urlopen(f"{ftp_path}/oa_{group}/xml/") as response:
         html = response.read().decode("utf-8")
     csvs = re.findall(rf"oa_{group}_xml\..*\.baseline\..*\.filelist\.csv", html)
     csvs = [csv.split(">")[-1] for csv in csvs]
     return csvs
 
 
-def get_local_baselines(group: str):
-    csvs = glob.glob(f"{LOCAL_PATH}/oa_{group}/xml/oa_{group}_xml.*.baseline.*.filelist.csv")
+def get_local_baselines(local_path: str, group: str):
+    csvs = glob.glob(f"{local_path}/oa_{group}/xml/oa_{group}_xml.*.baseline.*.filelist.csv")
     csvs = [csv.split("/")[-1] for csv in csvs]
     return csvs
 
 
-async def sync_incr(date: str, group: str, sem: Semaphore):
+async def sync_incr(ftp_path: str, local_path: str, date: str, group: str, sem: Semaphore):
     with tempfile.NamedTemporaryFile() as tmp:
-        url = f"{FTP_PATH}/oa_{group}/xml/oa_{group}_xml.incr.{date}.tar.gz"
+        url = f"{ftp_path}/oa_{group}/xml/oa_{group}_xml.incr.{date}.tar.gz"
         timeout = aiohttp.ClientTimeout(total=None)
         async with sem, aiohttp.ClientSession(timeout=timeout) as session:
             logger.info(f"{url} -> {tmp.name}")
@@ -98,7 +101,7 @@ async def sync_incr(date: str, group: str, sem: Semaphore):
                             await f.write(chunk)
                 else:
                     raise Exception(f"Failed to download {url}")
-        args = ["tar", "-xzvf", tmp.name, "-C", f"{LOCAL_PATH}/oa_{group}/xml/"]
+        args = ["tar", "-xzvf", tmp.name, "-C", f"{local_path}/oa_{group}/xml/"]
         logger.info(" ".join(args))
         process = await asyncio.create_subprocess_exec(
             *args,
@@ -109,9 +112,9 @@ async def sync_incr(date: str, group: str, sem: Semaphore):
         if process.returncode != 0:
             logger.error(stdout)
             logger.error(stderr)
-            raise Exception(f"Failed to extract {tmp.name} to {LOCAL_PATH}/oa_{group}/xml/")
-    url = f"{FTP_PATH}/oa_{group}/xml/oa_{group}_xml.incr.{date}.filelist.csv"
-    filename = f"{LOCAL_PATH}/oa_{group}/xml/oa_{group}_xml.incr.{date}.filelist.csv"
+            raise Exception(f"Failed to extract {tmp.name} to {local_path}/oa_{group}/xml/")
+    url = f"{ftp_path}/oa_{group}/xml/oa_{group}_xml.incr.{date}.filelist.csv"
+    filename = f"{local_path}/oa_{group}/xml/oa_{group}_xml.incr.{date}.filelist.csv"
     logger.info(f"{url} -> {filename}")
     timeout = aiohttp.ClientTimeout(total=None)
     async with aiohttp.ClientSession(timeout=timeout) as session:
@@ -123,23 +126,24 @@ async def sync_incr(date: str, group: str, sem: Semaphore):
                 raise Exception(f"Failed to download {url}")
 
 
-def get_local_dates(group):
-    csvs = glob.glob(f"{LOCAL_PATH}/oa_{group}/xml/oa_{group}_xml.incr.*.filelist.csv")
+def get_local_dates(local_path: str, group: str):
+    csvs = glob.glob(f"{local_path}/oa_{group}/xml/oa_{group}_xml.incr.*.filelist.csv")
     dates = [date.fromisoformat(csv.split(".")[-3]) for csv in csvs]
     return dates
 
 
-def get_remote_dates(group: str):
-    with urllib.request.urlopen(f"{FTP_PATH}/oa_{group}/xml/") as response:
+def get_remote_dates(ftp_path: str, group: str):
+    with urllib.request.urlopen(f"{ftp_path}/oa_{group}/xml/") as response:
         html = response.read().decode("utf-8")
     dates = [
-        date.fromisoformat(csv) for csv in re.findall(rf"oa_{group}_xml\.incr\.(\d{{4}}-\d{{2}}-\d{{2}})\.filelist\.csv", html)
+        date.fromisoformat(csv)
+        for csv in re.findall(rf"oa_{group}_xml\.incr\.(\d{{4}}-\d{{2}}-\d{{2}})\.filelist\.csv", html)
     ]
     return dates
 
 
-async def sync_incrs(group: str):
-    local_latest_dates = get_local_dates(group)
+async def sync_incrs(ftp_path: str, local_path: str, group: str):
+    local_latest_dates = get_local_dates(local_path, group)
     remote_latest_dates = get_remote_dates(group)
     dates_delta = sorted(set(remote_latest_dates) - set(local_latest_dates))
     if not dates_delta:
@@ -154,7 +158,23 @@ async def sync_incrs(group: str):
 async def main():
     # TODO: logs broken..
     # TODO: highly duplicated code
-    # TODO: highly coupled to file stucture
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ftp_path",
+        type=str,
+        default="https://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk",
+        help="FTP path to download PMC OA bulk",
+    )
+    parser.add_argument(
+        "--local_path",
+        type=str,
+        default="/data/ftp.ncbi.nlm.nih.gov/pub/pmc/oa_bulk",
+        help="Local path to download PMC OA bulk",
+    )
+    args = parser.parse_args()
+    ftp_path = args.ftp_path
+    local_path = args.local_path
+
     Path("logs").mkdir(parents=True, exist_ok=True)
     filename = f"logs/{Path(__file__).stem}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
     logging.basicConfig(
@@ -168,8 +188,8 @@ async def main():
     logger.addHandler(console)
     groups = ["comm", "noncomm", "other"]
     for group in groups:
-        await sync_baselines(group)
-        await sync_incrs(group)
+        await sync_baselines(ftp_path, local_path, group)
+        await sync_incrs(ftp_path, local_path, group)
 
 
 if __name__ == "__main__":
