2e0cbf1a2367e75384e2bc4a820e5e899a7f2360
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-04-08 18:49:09 +0000

    Fix encoding bug

--- src/get_list_of_rare_diseases.py
+++ src/get_list_of_rare_diseases.py
@@ -7,7 +7,7 @@ def main():
     rare_diseases = []
 
     # https://www.orphadata.com/data/xml/en_product7.xml
-    with open("data/en_product7.xml", "r", encoding="ISO-8859-1") as f:
+    with open("data/en_product7.xml", "rb") as f:
         root = ET.parse(f).getroot()
         for disorder in root.findall(".//Disorder"):
             name = disorder.find("Name").text
@@ -17,7 +17,7 @@ def main():
     df = df.drop_duplicates()
     # for terms containing OR, we need to wrap them in parentheses
     df["rare_disease"] = df["rare_disease"].str.replace(r"([\w-]+ or [\w]+)", r"(\1)", regex=True)
-    df.to_csv("data/rare_diseases.csv", index=False)
+    df.to_csv("data/rare_diseases.csv", index=False, encoding="utf-8")
 
 
 if __name__ == "__main__":
