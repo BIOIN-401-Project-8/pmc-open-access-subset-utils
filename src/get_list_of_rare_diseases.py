69c71ab13f15974243223016688d33405250dacf
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-04-08 17:33:05 +0000

    Get all rare diseases from orphadata, also removes noise like "Clinical Group"

---
+++ src/get_list_of_rare_diseases.py
@@ -0,0 +1,22 @@
+import xml.etree.ElementTree as ET
+
+import pandas as pd
+
+
+def main():
+    rare_diseases = []
+
+    # https://www.orphadata.com/data/xml/en_product7.xml
+    with open("data/en_product7.xml", "r", encoding="latin-1") as f:
+        root = ET.parse(f).getroot()
+        for disorder in root.findall(".//Disorder"):
+            name = disorder.find("Name").text
+            rare_diseases.append(name)
+
+    df = pd.DataFrame(rare_diseases, columns=["rare_disease"])
+    df = df.drop_duplicates()
+    df.to_csv("data/rare_diseases.csv", index=False)
+
+
+if __name__ == "__main__":
+    main()
