6a20df6fe1338ca44c2906387b1ca1bf67f5c412
Steven Tang <18170455+steventango@users.noreply.github.com>
2024-04-08 16:49:33 +0000

    Refactoring

--- src/generate_plots.py
+++ src/generate_plots.py
@@ -5,13 +5,13 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from utils import get_esearch_count
+from utils import get_esearch_key
 
 # %%
 esearchs = glob("/data/pmc-open-access-subset/esearch/*.xml")
 dfs = []
 for esearch in esearchs:
-    esearch_count = get_esearch_count(esearch)
+    esearch_count = get_esearch_key(esearch)
     dfs.append(pd.DataFrame({"count": [esearch_count], "disease": [Path(esearch).stem]}))
 df = pd.concat(dfs)
 df = df.sort_values("count", ascending=False)
